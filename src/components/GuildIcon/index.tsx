8dea3bc316dc24170301c2d0b9876e0f948f0225
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-06-22 22:27:42 -0300

    home screen

---
+++ src/components/GuildIcon/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Image } from 'react-native';
+import {RectButton, RectButtonProps} from 'react-native-gesture-handler'
+import {styles} from './styles'
+
+export function GuildIcon(){
+
+    return(
+        <Image source={{uri:'https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2020/04/discord-logo-1000x563.jpg'}} style={styles.image} resizeMode='cover'/>
+    )
+}

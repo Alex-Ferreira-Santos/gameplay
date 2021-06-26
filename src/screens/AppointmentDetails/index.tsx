b450af5af3d773815d27e8cc38772c58a9429fec
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-06-26 13:28:02 -0300

    member select and share

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {ImageBackground, Text, View, FlatList} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {ImageBackground, Text, View, FlatList, Alert, Share, Platform} from 'react-native'
 import {Fontisto} from '@expo/vector-icons'
 import {BorderlessButton} from 'react-native-gesture-handler'
 
@@ -7,43 +7,67 @@ import {BorderlessButton} from 'react-native-gesture-handler'
 import {Background} from '../../components/Background'
 import {ListHeader} from '../../components/ListHeader'
 import {Header} from '../../components/Header'
-import { Member } from '../../components/Member'
+import { Member, MemberProps } from '../../components/Member'
 import {ListDivider} from '../../components/ListDivider'
 import {ButtonIcon} from '../../components/ButtonIcon'
 import { theme } from '../../global/styles/theme'
+import {Load} from '../../components/Load/Load'
 
 import BannerImg from '../../assets/banner.png'
 
 import {styles} from './styles'
+import { useRoute } from '@react-navigation/native'
+import { AppointmentProps } from '../../components/Appointment'
+import { api } from '../../services/api'
 
+type Params = {
+    guildSelect: AppointmentProps
+}
+
+type GuildWidget = {
+    id: string,
+    name: string,
+    instant_invite: string,
+    members:MemberProps[],
+}
 
 export function AppointmentDetails(){
-    const members = [
-        {
-            id:'1', 
-            username: 'Alex',
-            avatar_url:'https://github.com/Alex-Ferreira-Santos.png',
-            status: 'online',
-        },
-        {
-            id:'2', 
-            username: 'Alex',
-            avatar_url:'https://github.com/Alex-Ferreira-Santos.png',
-            status: 'offline',
-        },
-        {
-            id:'3', 
-            username: 'Alex',
-            avatar_url:'https://github.com/Alex-Ferreira-Santos.png',
-            status: 'online',
+    const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget)
+    const [loading,setLoading] = useState(true)
+    const {params} = useRoute()
+    const {guildSelect} = params as Params
+
+    async function fecthGuildWidget(){
+        try{
+            const response = await api.get(`/guilds/${guildSelect.guild.id}/widget.json`)
+            setWidget(response.data)
+        }catch(error){
+            Alert.alert('Verifique as configurações do servidor. Será que o Widget está habilitado')
+        } finally{
+            setLoading(false)
         }
-    ]
+    }
+
+    function handleShareInvitation(){
+        const message = Platform.OS === 'ios' ? `Junte-se a ${guildSelect.guild.name}` : widget.instant_invite
+
+            Share.share({
+                message,
+                url: widget.instant_invite
+            })
+    
+    }
+
+    useEffect( () => {
+        fecthGuildWidget()
+    },[])
+    
     return (
         <Background>
             <Header 
                 title='Detalhes'
                 action={
-                    <BorderlessButton>
+                    guildSelect.guild.owner && <BorderlessButton onPress={handleShareInvitation}>
                         <Fontisto
                             name='share'
                             size={24}
@@ -58,26 +82,26 @@ export function AppointmentDetails(){
                 style={styles.banner}
             >
                 <View style={styles.bannerContent}>
-                    <Text style={styles.title}>Lendários</Text>
-                    <Text style={styles.title}>É hoje que vamos chegar ao challenger sem perder uma partida da md10</Text>  
+                    <Text style={styles.title}>{guildSelect.guild.name}</Text>
+                    <Text style={styles.subtitle}>{guildSelect.description}</Text>  
                 </View>
                 
             </ImageBackground>
 
-            <ListHeader
+            {loading ? <Load/> : <><ListHeader
                 title='Jogadores'
-                subtitle='Total 3'
+                subtitle={`Total ${widget.members.length}`}
             />
 
             <FlatList
-                data={members}
+                data={widget.members}
                 keyExtractor={ item => item.id}
                 renderItem={({item}) => (
                     <Member data={item}/>
                 )}
                 ItemSeparatorComponent={ () => <ListDivider isCentered/>}
                 style={ styles.members}
-            />
+            /></>}
             <View style={styles.footer}>
                 <ButtonIcon title='Entrar na partida'/>
             </View>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,60 +1,56 @@
-import React,{useState} from 'react';
+import React,{useState, useCallback} from 'react';
 import { View, FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import {styles} from './styles'
 
 import { CategorySelect } from '../../components/categorySelect'
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
 import {Background} from '../../components/Background'
+import {Load} from '../../components/Load/Load'
 
 import { ListHeader } from '../../components/ListHeader';
-import { Appointment } from '../../components/Appointment';
+import { Appointment, AppointmentProps } from '../../components/Appointment';
 import ListDivider from '../../components/ListDivider';
+import { COLLECTION_APPOINTMENTS } from '../../config/database';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export function Home(){
     const [category,setCategory] = useState('')
+    const [loading,setLoading] = useState(true)
+    const [appointment,setAppointment] = useState<AppointmentProps[]>([])
     const navigation = useNavigation()
 
-    const appointment = [
-        {
-            id:'1',
-            guild:{
-                id:'1',
-                name:'Lendários',
-                icon: null,
-                owner: true
-            },
-            category: '1',
-            date: '22/06 às 20:40h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-        },
-        {
-            id:'2',
-            guild:{
-                id:'1',
-                name:'Lendários',
-                icon: null,
-                owner: true
-            },
-            category: '1',
-            date: '22/06 às 20:40h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-        }
-    ]
 
     function handleCategorySelect(categoryId: string){
         categoryId === category ? setCategory('') : setCategory(categoryId)
     }
 
-    function handleAppointmentDetails(){
-        navigation.navigate('AppointmentDetails')
+    function handleAppointmentDetails(guildSelect: AppointmentProps){
+        navigation.navigate('AppointmentDetails',{guildSelect})
     }
 
     function handleAppointmentCreate(){
         navigation.navigate('AppointmentCreate')
     }
+
+    async function loadAppointments(){
+        const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS)
+        const storage: AppointmentProps[] = response ? JSON.parse(response) : []
+
+        if(category){
+            setAppointment(storage.filter( item => item.category === category))
+        }else{
+            setAppointment(storage)
+        }
+
+        setLoading(false)
+    }
+
+    useFocusEffect(useCallback( () => {
+        loadAppointments()
+    },[category]))
 
     return(
         <Background>
@@ -64,30 +60,31 @@ export function Home(){
                     <ButtonAdd onPress={handleAppointmentCreate}/>
                 </View>
 
-                <View>
-                    <CategorySelect
-                        categorySelected={category}
-                        setCategory={handleCategorySelect}
-                    />
+                
+            <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
+            {loading ? <Load/> : 
+            <>
+                <ListHeader title='Partidas agendadas' subtitle={`Total ${appointment.length}`}/>
 
-                    <ListHeader title='Partidas agendadas' subtitle='Total 6'/>
-
-                    <FlatList
-                            data={appointment}
-                            keyExtractor={item => item.id}
-                            renderItem={({item}) => (
-                                <Appointment 
-                                    data={item}
-                                    onPress={handleAppointmentDetails}
-                                />
-                            )}
-                            ItemSeparatorComponent={() => <ListDivider/>}
-                            contentContainerStyle={{paddingBottom: 69}}
-                            style={styles.matches}
-                            showsVerticalScrollIndicator={false}
+                <FlatList
+                    data={appointment}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) => (
+                        <Appointment 
+                            data={item}
+                            onPress={() =>handleAppointmentDetails(item)}
                         />
-                    </View>
-                </View>
+                    )}
+                    ItemSeparatorComponent={() => <ListDivider/>}
+                    contentContainerStyle={{paddingBottom: 69}}
+                    style={styles.matches}
+                    showsVerticalScrollIndicator={false}
+                />
+            </>}
+        </View>    
         </Background>
     )
 }
